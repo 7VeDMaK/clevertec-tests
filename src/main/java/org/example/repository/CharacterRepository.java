8f51a2c4db9497c2c597939d578e967840853c5f
Balay D. <darvinbin@gmail.com>
2024-09-14 17:30:03 +0300

    Added CRUD system

--- src/main/java/org/example/repository/CharacterRepository.java
+++ src/main/java/org/example/repository/CharacterRepository.java
@@ -1,4 +1,38 @@
 package org.example.repository;
 
+import org.example.common.CharacterType;
+import org.example.entity.CharacterEntity;
+
+import java.util.List;
+import java.util.UUID;
+
 public class CharacterRepository {
+
+    private static final List<CharacterEntity> db = List.of(
+            new CharacterEntity(UUID.randomUUID(), "Gigachad", 18, CharacterType.SHIELDER),
+            new CharacterEntity(UUID.randomUUID(), "Gigakid", 79, CharacterType.ARCHER),
+            new CharacterEntity(UUID.randomUUID(), "Gigalaptop", 132, CharacterType.ALTER_EGO),
+            new CharacterEntity(UUID.randomUUID(), "Sigmamale", 44, CharacterType.ASSASSIN),
+            new CharacterEntity(UUID.randomUUID(), "Radiatedkid", 19, CharacterType.BEAST)
+    );
+
+    public List<CharacterEntity> getCharacters() {
+        return db;
+    }
+
+    public CharacterEntity getCharacter(UUID id) {
+        return db.getFirst();
+    }
+
+    public CharacterEntity createCharacter(CharacterEntity character) {
+        return character;
+    }
+
+    public CharacterEntity updateCharacter(UUID id, CharacterEntity character) {
+        character.setId(id);
+        return character;
+    }
+
+    public void deleteCharacter(UUID id) {
+    }
 }
--- src/main/java/org/example/service/CharacterService.java
+++ src/main/java/org/example/service/CharacterService.java
@@ -1,4 +1,47 @@
 package org.example.service;
 
+import org.example.domain.Character;
+import org.example.entity.CharacterEntity;
+import org.example.mapper.CharacterMapper;
+import org.example.mapper.CharacterMapperImpl;
+import org.example.repository.CharacterRepository;
+
+import java.util.List;
+import java.util.UUID;
+
 public class CharacterService {
+
+    private final CharacterRepository characterRepository = new CharacterRepository();
+
+    private final CharacterMapper characterMapper = new CharacterMapperImpl();
+
+    public List<Character> getCharacters() {
+        List<CharacterEntity> characters = characterRepository.getCharacters();
+
+        return characterMapper.toDomains(characters);
+    }
+
+    public Character getCharacter(UUID id) {
+        CharacterEntity character = characterRepository.getCharacter(id);
+
+        return characterMapper.toDomain(character);
+    }
+
+    public Character createCharacter(Character character) {
+        CharacterEntity characterEntity = characterMapper.toEntity(character);
+        CharacterEntity createdEntity = characterRepository.createCharacter(characterEntity);
+
+        return characterMapper.toDomain(createdEntity);
+    }
+
+    public Character updateCharacter(UUID id, Character character) {
+        CharacterEntity characterEntity = characterMapper.toEntity(character);
+        CharacterEntity updatedEntity = characterRepository.updateCharacter(id, characterEntity);
+
+        return characterMapper.toDomain(updatedEntity);
+    }
+
+    public void deleteCharacter(UUID id) {
+        characterRepository.deleteCharacter(id);
+    }
 }
